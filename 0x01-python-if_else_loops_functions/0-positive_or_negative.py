8c574cad310d41b4880d5d190d5631e30844f300
Kheireddine Anas <kheireddine.anas@gmail.com>
2023-06-06 14:30:01 +0100

    Create 0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(number, "is", end=" ")
+if number > 0:
+    print("positive")
+elif number == 0:
+    print("zero")
+else:
+    print("negative")
